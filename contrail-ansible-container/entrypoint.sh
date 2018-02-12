840fef72693fa4d6f671c3889d1f72b9b970fea6
Michael Henkel <mhenkel@juniper.net>
2018-02-12 11:41:56 -0800

    fixes

--- contrail-ansible-container/entrypoint.sh
+++ contrail-ansible-container/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 git clone http://github.com/michaelhenkel/contrail-ansible-deployer
 mkdir /configs
-echo $INSTANCES | jq . > /configs/instances.json
-echo $ACCOUNTS | jq . > /configs/accounts.json
+echo $INSTANCES | jq . > /configs/instances
 echo $GCE | jq . > /configs/gce.json
 echo "[defaults]" > /etc/ansible/ansible.cfg
 echo "host_key_checking = False" >> /etc/ansible/ansible.cfg
